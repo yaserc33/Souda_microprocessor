0acf232d3b0ba8c075f78b325752f2319590f5a5
cenepi <raltamimi0001@stu.kau.edu.sa>
2024-11-05 23:29:13 -0800

    V2.0.0

---
+++ assemblerV2.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int main(int argc, char* chrFileName[]){
+	char	chrBuff[10];
+	char	chrBuffOp[2];
+	char	chrBuffOperand1[2];
+	char	chrBuffOperand2[2];
+	char	chrBuff2[20];
+	char	chrBuffForOp[20];
+	char	chrCode[1];
+
+	FILE *ptrSourceFile;
+	FILE *ptrDestinationFile;
+
+	int	intJ		=0;
+	int	intC		=0;
+	int	intD1		=0;
+	int	intD0		=0;
+	int	intSreg		=0;
+	int 	intMinus	=0;
+	int	intLiteralDigit2=0;
+	int	intLiteralDigit1=0;
+	int	intLiteralDigit0=0;
+
+	int	intOpCounter	=0;
+
+	ptrSourceFile=fopen(chrFileName[1],"r");
+	if(ptrSourceFile==NULL){
+		printf("ERROR OPENING THE SOURCE FILE");
+		return 1;
+	}
+
+	ptrDestinationFile=fopen("fibonacci.bin","w");
+	if(ptrSourceFile==NULL){
+		printf("ERROR OPENING THE DESTINATION FILE");
+		return 1;
+	}
+
+
+	//convert the whole file into uppercase
+	while(fgets(chrBuff,sizeof(chrBuff),ptrSourceFile)!=NULL){
+                for(int a=0;a<sizeof(chrBuff)-1;a++){
+                        if(chrBuff[a]!='\n'){
+				chrBuff2[a]=chrBuff[a];
+
+			}
+			else{
+				break;
+			}
+		}
+		strcpy(chrBuffForOp,chrBuff2);
+		char* line = strtok(chrBuffForOp,"=");
+		line=strtok(chrBuffForOp,"-");
+		while(line !=NULL){
+			chrBuffOp[0]=line[0];
+			chrBuffOp[1]=line[1];
+			line=strtok(NULL,"+");
+		}
+		int m=0;
+		while(chrBuff2[m]!='='){
+			m++;
+		}
+		if(toupper(chrBuff2[m+1])=='R'){
+			intSreg=0;
+			if(chrBuff2[m+3]=='+'){
+				intLiteralDigit2=0;
+			}
+			else if(chrBuff2[m+3]=='-'){
+				intLiteralDigit2=1;
+			}
+			else if(chrBuff2[m+3]==NULL){
+                                if      (toupper(chrBuffOp[1])=='A'){
+                                        printf("INVALID INPUT!");
+					return 1;
+                                }
+                                else if (toupper(chrBuffOp[1])=='B'){
+                                        printf("INVALID INPUT!");
+					return 1;
+                                }
+                                else if (toupper(chrBuffOp[1])=='O'){
+                                        intD1   =1;
+                                        intD0   =0;
+                                        intJ    =0;
+                                        intC    =0;
+                                }
+                                else{
+                                        printf("INVALID INPUT!");
+                                        return 1;
+                                }
+
+			}
+			else{
+				printf("INVALID INPUT!");
+				return 1;
+			}
+		}
+		else if((int)chrBuff2[m+1]>47 &&(int)chrBuff2[m+1]<56){
+			intSreg=1;
+			switch ( (int)chrBuff2[m+1] ){
+				case 48:
+					intLiteralDigit2=0;
+					intLiteralDigit1=0;
+					intLiteralDigit0=0;
+					break;
+                                case 49:
+                                        intLiteralDigit2=0;
+                                        intLiteralDigit1=0;
+                                        intLiteralDigit0=1;
+                                        break;
+
+                                case 50:
+                                        intLiteralDigit2=0;
+                                        intLiteralDigit1=1;
+                                        intLiteralDigit0=0;
+                                        break;
+
+                                case 51:
+                                        intLiteralDigit2=0;
+                                        intLiteralDigit1=1;
+                                        intLiteralDigit0=1;
+                                        break;
+
+                                case 52:
+                                        intLiteralDigit2=1;
+                                        intLiteralDigit1=0;
+                                        intLiteralDigit0=0;
+                                        break;
+
+                                case 53:
+                                        intLiteralDigit2=1;
+                                        intLiteralDigit1=0;
+                                        intLiteralDigit0=1;
+                                        break;
+
+                                case 54:
+                                        intLiteralDigit2=1;
+                                        intLiteralDigit1=1;
+                                        intLiteralDigit0=0;
+                                        break;
+
+                                case 55:
+                                        intLiteralDigit2=1;
+                                        intLiteralDigit1=1;
+                                        intLiteralDigit0=1;
+                                        break;
+				default:
+					printf("INVALID INPUT!");
+					return 1;
+			}
+
+		}
+		else{
+			printf("INVALID INPUT!");
+			return 1;
+		}
+			if(toupper(chrBuffOp[0])=='R'){
+				if	(toupper(chrBuffOp[1])=='A'){
+					intD1	=0;
+					intD0	=0;
+					intJ	=0;
+					intC	=0;
+				}
+				else if	(toupper(chrBuffOp[1])=='B'){
+					intD1	=0;
+					intD0	=1;
+					intJ	=0;
+					intC	=0;
+				}
+				else if	(toupper(chrBuffOp[1])=='O'){
+					intD1	=1;
+					intD0	=0;
+					intJ	=0;
+					intC	=0;
+				}
+				else{
+					printf("INVALID INPUT!");
+					return 1;
+				}
+			}
+			else if(toupper(chrBuffOp[0])=='J'){
+				 if(chrBuffOp[1]==NULL){
+					intD1	=0;
+					intD0	=0;
+					intJ	=1;
+					intC	=0;
+				}
+				else if(toupper(chrBuffOp[1])=='C'){
+					intD1	=0;
+					intD0	=0;
+					intJ	=0;
+					intC	=1;
+				}
+				else{
+					printf("INVALID INPUT!");
+					return 1;
+				}
+			}
+                        fprintf(ptrDestinationFile,"%d%d%d%d%d%d%d%d\n",intJ,intC,intD1,intD0,intSreg,intLiteralDigit2,intLiteralDigit1,intLiteralDigit0);
+	}
+fclose(ptrSourceFile);
+fclose(ptrDestinationFile);
+return 0;
+}
+
---
+++ fibonacci.asm
@@ -0,0 +1,9 @@
+RA=0
+RB=1
+RO=RA
+RB=RA+RB
+JC=0
+RA=RA+RB
+RB=RA-RB
+RA=RA-RB
+J=2
